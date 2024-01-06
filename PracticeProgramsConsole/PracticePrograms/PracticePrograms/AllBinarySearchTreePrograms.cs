84d8fc5f01c8f05543cff91e73724c41b6f47a82
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-01-06 20:05:41 +0530

    PRACTICE : 06/01/2024

---
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/AllBinarySearchTreePrograms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePrograms
+{
+    internal class AllBinarySearchTreePrograms
+    {
+        public int minValue(Node root)
+        {
+            // code here
+            int ans = -1;
+            MinValueHelper(root, ref ans);
+            return ans;
+        }
+        private void MinValueHelper(Node root, ref int ans)
+        {
+            if (root == null)
+                return;
+            MinValueHelper(root.left, ref ans);
+            if (ans == -1)
+                ans = root.data;
+        }
+    }
+}
