bdb602e0c463101a2a9e8b7cc62118c179fb28e1
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-01-08 21:19:59 +0530

    PRACTICE : 08/01/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/AllQueuePrograms.cs
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/AllQueuePrograms.cs
@@ -6,6 +6,37 @@ using System.Threading.Tasks;
 
 namespace PracticePrograms
 {
+    class MyQueue
+    {
+        //Your code here
+        private int[] arr;
+        private int front;
+        private int rear;
+
+        public MyQueue()
+        {
+            arr = new int[100005];
+            front = 0;
+            rear = 0;
+        }
+
+        public void push(int x)
+        {
+            //Your code here
+            if (rear == arr.Length - 1)
+                return;
+            arr[rear++] = x;
+        }
+
+        public int pop()
+        {
+            //Your code here
+            if (rear == 0 || front == rear)
+                return -1;
+            int t = arr[front++];
+            return t;
+        }
+    }
     internal class AllQueuePrograms
     {
     }
