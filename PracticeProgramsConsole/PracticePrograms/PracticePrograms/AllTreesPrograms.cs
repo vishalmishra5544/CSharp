bc125ea1933ff58e4ca0978534dba884b3060d07
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-01-11 21:20:42 +0530

    PRACTICE : 11/01/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/AllTreesPrograms.cs
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/AllTreesPrograms.cs
@@ -8,5 +8,22 @@ namespace PracticePrograms
 {
     internal class AllTreesPrograms
     {
+        //Function to return a list containing the postOrder traversal of the tree.
+        public List<int> postOrder(Node root)
+        {
+            //code here
+            List<int> ans = new List<int>();
+            HelperPostOrder(root, ans);
+            return ans;
+        }
+
+        private void HelperPostOrder(Node root, List<int> ans)
+        {
+            if (root == null)
+                return;
+            HelperPostOrder(root.left, ans);
+            HelperPostOrder(root.right, ans);
+            ans.Add(root.data);
+        }
     }
 }
