56b7f981a315f38f079d056bafd5c8effa98cb14
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-01-29 20:58:27 +0530

    PRACTICE : 29/01/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CSharp/AllBinarySearchPrograms.cs
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CSharp/AllBinarySearchPrograms.cs
@@ -25,6 +25,64 @@ namespace PracticePrograms
                     high = mid - 1;
             }
             return 0;
+        }       
+
+
+        public int count(int[] arr, int n, int x)
+        {
+            //Your code here
+            int firstOccurrence = FirstOccurrence(arr, n, x);
+            int lastOccurrence = LastOccurrence(arr, n, x);
+            if (firstOccurrence == -1 || lastOccurrence == -1)
+                return 0;
+            return lastOccurrence - firstOccurrence + 1;
+        }
+
+        private int FirstOccurrence(int[] arr, int n, int x)
+        {
+            int s = 0, e = n - 1;
+            int ans = -1;
+            while (s <= e)
+            {
+                int mid = s + (e - s) / 2;
+                if (arr[mid] == x)
+                {
+                    ans = mid;
+                    e = mid - 1;
+                }
+                else if (arr[mid] > x)
+                {
+                    e = mid - 1;
+                }
+                else
+                {
+                    s = mid + 1;
+                }
+            }
+            return ans;
+        }
+        private int LastOccurrence(int[] arr, int n, int x)
+        {
+            int s = 0, e = n - 1;
+            int ans = -1;
+            while (s <= e)
+            {
+                int mid = s + (e - s) / 2;
+                if (arr[mid] == x)
+                {
+                    ans = mid;
+                    s = mid + 1;
+                }
+                else if (arr[mid] > x)
+                {
+                    e = mid - 1;
+                }
+                else
+                {
+                    s = mid + 1;
+                }
+            }
+            return ans;
         }
     }
 }
