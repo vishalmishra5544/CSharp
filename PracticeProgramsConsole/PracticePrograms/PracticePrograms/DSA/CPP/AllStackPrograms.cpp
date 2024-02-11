95bdbd0e0c5377c6a88d4c89fe58905a84e1acaa
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-02-11 20:30:43 +0530

    PRACTICE : 11/02/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllStackPrograms.cpp
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllStackPrograms.cpp
@@ -30,6 +30,58 @@ bool ispar(string x)
         return true;
     }
 
+class twoStacks
+{
+    public:
+    int *arr=NULL;
+    int top1,top2;
+    int size;
+    twoStacks()
+    {
+        arr = new int[100];
+        size=100;
+        top1=-1;
+        top2=100;
+    }
+ 
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        top1++;
+        if(top1==size || top1>=top2)
+          return;
+        arr[top1]=x;
+    }
+    
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+        top2--;
+        if(top2==0 || top2<=top1)
+          return;
+        arr[top2]=x;
+    }
+    
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        if(top1<0)
+         return -1;
+        int ans = arr[top1];
+        top1--;
+        return ans;
+    }
+    
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        if(top2==100)
+         return -1;
+        int ans = arr[top2];
+        top2++;
+        return ans;
+    }
+};
     int main()
     {
         return 0;
