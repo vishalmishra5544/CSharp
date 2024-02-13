b6e99fda2708707db252093f94065e9c869d6f0f
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-02-13 22:08:31 +0530

    PRACTICE : 13/02/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllBinarySearchPrograms.cpp
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllBinarySearchPrograms.cpp
@@ -124,6 +124,52 @@ vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
            return lower_bound(arr,arr+n,1)-arr;
         return -1;
     }
+
+long long minTime(int arr[], int n, int k)
+    {
+        // code here
+        // return minimum time
+        //sort(arr,arr+n);
+        long long start=0,end=0,mid,ans=-1;
+        for(int i=0;i<n;i++)
+         end+=arr[i];
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(isPossibleToPaint(arr,n,k,mid))
+            {
+                ans = mid;
+                end=mid-1;
+            }
+            else
+            {
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    bool isPossibleToPaint(int arr[],int n,int k ,long long mid)
+    {
+        long long currLen=0,noOfWorkers=1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>mid)
+             return false;
+            if(currLen + arr[i] >mid)
+            {
+                currLen=arr[i];
+                noOfWorkers++;
+                if(noOfWorkers>k)
+                 return false;
+            }
+            else
+            {
+              currLen+=arr[i];
+                
+            }
+        }
+        return true;
+    }
 int main()
 {
 	return 0;
