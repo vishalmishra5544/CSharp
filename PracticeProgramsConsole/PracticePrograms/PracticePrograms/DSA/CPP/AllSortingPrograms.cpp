68239292ee56ae73c60f7a3b709c2d76ca89155f
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-02-14 21:58:49 +0530

    PRACTICE : 14/02/2024

---
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllSortingPrograms.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+//Function to sort the array using bubble sort algorithm.
+void bubbleSort(int arr[], int n)
+{
+    // Your code here  
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+int main()
+{
+	return 0;
+}
