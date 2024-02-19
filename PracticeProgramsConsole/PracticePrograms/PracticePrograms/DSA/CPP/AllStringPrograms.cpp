2cedf02a43d1ce529bfb86058cb7ee94cc596f16
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-02-19 21:52:56 +0530

    PRACTICE : 19/02/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllStringPrograms.cpp
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllStringPrograms.cpp
@@ -12,8 +12,23 @@ string removeSpaces (string s)
         return ans;
     }
 
-
-//Recursively remove all adjacent duplicates
+//Remove All adjacent duplicates (only 2)
+string removeAdjacentDuplicatesAtMax2(string s)
+{
+    string ans="";
+    for(int i=0;i<s.length();i++)
+    {
+        if(ans.length()>0 && s[i]==ans[ans.length()-1])
+        {
+            ans.pop_back();
+        }
+        else{
+            ans.push_back();
+        }
+    }
+    return ans;
+}
+//Recursively remove all adjacent duplicates (more than 2)
 string rremove(string s){
         // code here
         string s1=s;
@@ -45,6 +60,141 @@ string removeAdjacentDuplicates(string s){
      return ans;        
 }
 
+//https://www.geeksforgeeks.org/problems/game-with-string4100/1
+int minValue(string s, int k){
+        // code here
+        int freq[26]={0};
+        for(auto& ele:s)
+        {
+            freq[ele-'a']++;
+        }
+        sort(freq,freq+26);
+        for(int i=0;i<k;i++)
+        {
+            freq[25]--;
+            sort(freq,freq+26);
+        }
+        int ans=0;
+        for(int i=0;i<26;i++)
+        {
+            ans+=(freq[i]*freq[i]);
+        }
+        return ans;
+    }
+//Remove all occurrence of substring
+//using find();
+// string removeAllOccurreneOfSubstring(string s,string part)
+// {
+//     int pos=s.find(part);
+//     while(pos!=string::npos)
+//     {
+//         s.erase(pos,part.length());
+//         pos = s.find(part);
+//     }
+//     return s;
+// }
+//w/o using find()  https://www.geeksforgeeks.org/minimize-a-string-by-removing-all-occurrences-of-another-string/
+// Function to find the minimum length
+// to which string str can be reduced to
+// by removing all occurrences of string K
+int removeAllOccurreneOfSubstring(string str, int N,
+              string K, int M)
+{
+ 
+    // Initialize stack of characters
+    stack<char> stackOfChar;
+ 
+    for (int i = 0; i < N; i++) {
+ 
+        // Push character into the stack
+        stackOfChar.push(str[i]);
+ 
+        // If stack size >= K.size()
+        if (stackOfChar.size() >= M) {
+ 
+            // Create empty string to
+            // store characters of stack
+            string l = "";
+ 
+            // Traverse the string K in reverse
+            for (int j = M - 1; j >= 0; j--) {
+ 
+                // If any of the characters
+                // differ, it means that K
+                // is not present in the stack
+                if (K[j] != stackOfChar.top()) {
+ 
+                    // Push the elements
+                    // back into the stack
+                    int f = 0;
+                    while (f != l.size()) {
+ 
+                        stackOfChar.push(l[f]);
+                        f++;
+                    }
+ 
+                    break;
+                }
+ 
+                // Store the string
+                else {
+ 
+                    l = stackOfChar.top()
+                        + l;
+ 
+                    // Remove top element
+                    stackOfChar.pop();
+                }
+            }
+        }
+    }
+ 
+    // Size of stack gives the
+    // minimized length of str
+    return stackOfChar.size();
+}
+// VALID PALINDROME -2  https://www.geeksforgeeks.org/remove-character-string-make-palindrome/
+// This method returns -1 if it is not possible to make string
+// a palindrome. It returns -2 if string is already a palindrome.
+// Otherwise it returns index of character whose removal can
+// make the whole string palindrome.
+int possiblePalinByRemovingOneChar(string str)
+{
+    //  Initialize low and high by both the ends of the string
+    int low = 0, high = str.length() - 1;
+ 
+    //  loop until low and high cross each other
+    while (low < high)
+    {
+        // If both characters are equal then move both pointer
+        // towards end
+        if (str[low] == str[high])
+        {
+            low++;
+            high--;
+        }
+        else
+        {
+            /*  If removing str[low] makes the whole string palindrome.
+                We basically check if substring str[low+1..high] is
+                palindrome or not. */
+            if (isPalindrome(str.begin() + low + 1, str.begin() + high))
+                return low;
+ 
+            /*  If removing str[high] makes the whole string palindrome
+                We basically check if substring str[low+1..high] is
+                palindrome or not. */
+            if (isPalindrome(str.begin() + low, str.begin() + high - 1))
+                return high;
+ 
+            return -1;
+        }
+    }
+ 
+    //  We reach here when complete string will be palindrome
+    //  if complete string is palindrome then return mid character
+    return -2;
+}
 int main()
 {
 	return 0;
