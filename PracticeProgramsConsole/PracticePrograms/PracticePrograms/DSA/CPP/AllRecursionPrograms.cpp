d51e65f7e7135864961081fab484f9caf5436cbc
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-04-07 21:21:18 +0530

    PRACTICE : 07/04/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllRecursionPrograms.cpp
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/AllRecursionPrograms.cpp
@@ -73,6 +73,30 @@ bool isSafe(int r, int c, vector<vector<int>> &mat){
         // returning the result.
         return res[n];
     }
+//https://www.geeksforgeeks.org/problems/maximize-dot-product2649/1
+int maxDotProduct(int n, int m, int a[], int b[])
+{
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+    return solve(0, 0, n, m, a, b, dp);
+}
+int solve(int i, int j, int n, int m, int a[], int b[], vector<vector<int>> &dp)
+{
+    if (j == m)
+    {
+        return 0;
+    }
+    if (i == n)
+    {
+        return INT_MIN;
+    }
+    if (dp[i][j] != -1)
+    {
+        return dp[i][j];
+    }
+    int no_take = 0 + solve(i + 1, j, n, m, a, b, dp);
+    int take = a[i] * b[j] + solve(i + 1, j + 1, n, m, a, b, dp);
+    return dp[i][j] = max(no_take, take);
+}
 int main()
 {
     vector<vector<int>> mat(5,vector<int> (5,1));
