c711f1e2ab1a19cb85a3a223893036e4d725a43f
vishalmishra5544 <vishalmishra5544@gmail.com>
2024-05-26 20:31:10 +0530

    PRACTICE : 26/05/2024

--- PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/potd.cpp
+++ PracticeProgramsConsole/PracticePrograms/PracticePrograms/DSA/CPP/potd.cpp
@@ -59,6 +59,55 @@ long long max_Books(int arr[], int n, int k) {
        }
        return maxi;
     }
+//https://www.geeksforgeeks.org/problems/minimum-cost-to-make-two-strings-identical1107/1
+//Memoization
+    // int fun(string &s1,string &s2,int c1,int c2,int i,int j, vector<vector<int>> &dp)
+    // {
+    //     if(i==s1.length() && j==s2.length())
+    //     {
+    //         return 0;
+    //     }
+    //     if(i==s1.length() && j!=s2.length())
+    //     {
+    //         return (s2.length()-j)*c2;
+    //     }
+    //     if(i!=s1.length() && j==s2.length())
+    //     {
+    //         return (s1.length()-i)*c1;
+    //     }
+        
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
+        
+    //     int cost=0;
+    //     if(s1[i]==s2[j])
+    //         cost=fun(s1,s2,c1,c2,i+1,j+1,dp);
+    //     else 
+    //         cost=min(c1+fun(s1,s2,c1,c2,i+1,j,dp),c2+fun(s1,s2,c1,c2,i,j+1,dp));
+        
+    //     return dp[i][j]=cost;
+    // }
+    //Tabulation
+    int findMinCost(string X, string Y, int costX, int costY) {
+        
+        int n=X.size();
+        int m=Y.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(X[i-1]==Y[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        int c1=(n-dp[n][m])*costX;
+        int c2=(m-dp[n][m])*costY;
+        return c1+c2;
+     
+    }
 int main()
 {
 	return 0;
